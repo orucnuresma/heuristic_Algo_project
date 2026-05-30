ab0dc6032ebe4d9d88465d553e068ac90a6bd4a2
serpilelinc <serpil.elinc@stu.fsm.edu.tr>
2026-05-30 13:53:27 +0300

    sample menu

---
+++ menu_converter.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import ast
+
+from database.data_loader import load_foods
+
+# Menü dosyası
+menus = pd.read_csv(
+    "results/sample_menus/algo_nsga2_menu_run1.csv"
+)
+
+# Veritabanından foods tablosunu çek
+foods = load_foods()
+
+food_dict = dict(zip(foods["id"], foods["name"]))
+
+row = menus.iloc[0]
+
+breakfast_ids = ast.literal_eval(row["breakfast_ids"])
+lunch_ids = ast.literal_eval(row["lunch_ids"])
+
+sample_breakfast = breakfast_ids[:7]
+sample_lunch = lunch_ids[:10]
+
+table = pd.DataFrame({
+    "Breakfast": [food_dict[i] for i in sample_breakfast] + [""] * 3,
+    "Lunch/Dinner": [food_dict[i] for i in sample_lunch]
+})
+
+print(table)
+table.to_csv(
+    "sample_menu_table.csv",
+    index=False,
+    encoding="utf-8-sig"
+)
+
+print("csv dosyası oluşturuldu: sample_menu_table.csv")
